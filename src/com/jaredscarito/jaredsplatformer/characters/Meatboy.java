712dc1f584c8c98fdfa50e8c2e58258719eedbdf
TheWolfBadger <TheWolfBadger@gmail.com>
2017-12-01 11:29:04 -0500

    - Added Storage Constructor class in which saves x and y pixels along with their color. - Character 'Meatboy' now uses Storage Constructor class in order to save pixels.

--- src/com/jaredscarito/jaredsplatformer/characters/Meatboy.java
+++ src/com/jaredscarito/jaredsplatformer/characters/Meatboy.java
@@ -1,9 +1,12 @@
 package com.jaredscarito.jaredsplatformer.characters;
 
 import com.jaredscarito.jaredsplatformer.objects.GameObject;
+import com.jaredscarito.jaredsplatformer.storage.Storage;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -20,10 +23,9 @@ public class Meatboy extends CharacterCollideable implements GameCharacter {
     private BufferedImage canvas;
     private Color[] cantCollide;
 
-    //TODO FOR REVAMP
-    private HashMap<HashMap<Integer, Integer>, Integer> irrelevantPoints;
-    private HashMap<Integer, Integer> leftArm;
-    private HashMap<Integer, Integer> rightArm;
+    private ArrayList<Storage> irrPoints = new ArrayList<>();
+    private ArrayList<Storage> leftArm = new ArrayList<>();
+    private ArrayList<Storage> rightArm = new ArrayList<>();
 
     public Meatboy(int startX, int startY, Graphics g, BufferedImage canvas, int speed, boolean isMoveable, Color... cantCollide) {
         this.isMoveable = isMoveable;
@@ -41,19 +43,14 @@ public class Meatboy extends CharacterCollideable implements GameCharacter {
         g.fillRect(rightFootStart, startY, 8, 8);
         this.legsY = startY + 8;
         //Add the points to array
-        HashMap<Integer, Integer> points = new HashMap<>();
         for(int y=startY; y<(startY + 8); y++) {
             //Left foot points
             for(int xLeft=startX; xLeft<(startX + 8); xLeft++) {
-                if(canvas.getRGB(xLeft, y) == Color.RED.getRGB()) {
-                    points.put(xLeft, y);
-                }
+                this.irrPoints.add(new Storage(xLeft, y, Color.RED));
             }
             //Right foot points
             for(int xRight=rightFootStart; xRight<(rightFootStart + 8); xRight++) {
-                if(canvas.getRGB(xRight, y) == Color.RED.getRGB()) {
-                    points.put(xRight, y);
-                }
+                this.irrPoints.add(new Storage(xRight, y, Color.RED));
             }
         }
 
@@ -66,13 +63,9 @@ public class Meatboy extends CharacterCollideable implements GameCharacter {
         //Add the points to array
         for(int x=bodyStartX; x<(bodyStartX + 24); x++) {
             for(int y=bodyStartY; y<(bodyStartY + 24); y++) {
-                if(canvas.getRGB(x, y) == Color.RED.getRGB()) {
-                    points.put(x, y);
-                }
+                this.irrPoints.add(new Storage(x, y, Color.RED));
             }
         }
-        this.irrelevantPoints = new HashMap<>();
-        this.irrelevantPoints.put(points, Color.RED.getRGB());
 
 
         //Eyes
@@ -86,16 +79,14 @@ public class Meatboy extends CharacterCollideable implements GameCharacter {
         g.drawRect(eyesStartX + 13, eyesStartY, 5, 5);
         g.fillRect(eyesStartX + 13, eyesStartY, 5, 5);
         //Add the points
-        HashMap<Integer, Integer> eyePoints = new HashMap<>();
-        for(int y=eyesStartY; y<(eyesStartY - 18); y++) {
+        for(int y=eyesStartY; y<(eyesStartY + 5); y++) {
             for(int xLeft = eyesStartX; xLeft<(eyesStartX + 5); xLeft++) {
-                eyePoints.put(xLeft, y);
+                this.irrPoints.add(new Storage(xLeft, y, Color.BLACK));
             }
             for(int xRight = (eyesStartX + 13); xRight<(eyesStartX + 13 + 5); xRight++) {
-                eyePoints.put(xRight, y);
+                this.irrPoints.add(new Storage(xRight, y, Color.BLACK));
             }
         }
-        this.irrelevantPoints.put(eyePoints, Color.BLACK.getRGB());
 
 
         //Arms
@@ -108,19 +99,17 @@ public class Meatboy extends CharacterCollideable implements GameCharacter {
         //Right
         g.drawRect(armsStartX + 28, armsStartY, 5, 8);
         g.fillRect(armsStartX + 28, armsStartY, 5, 8);
-        //add the points to arrays, armPointsLeftX, armPointsLeftY, armPointsRightX, armPointsRightY
-        HashMap<Integer, Integer> leftPoints = new HashMap<>();
-        HashMap<Integer, Integer> rightPoints = new HashMap<>();
+        //add the points
         //Left
         for(int xLeft=armsStartX; xLeft<(armsStartX + 5); xLeft++) {
             for(int yLeft=armsStartY; yLeft<(armsStartY + 8); yLeft++) {
-                leftPoints.put(xLeft, yLeft);
+                this.leftArm.add(new Storage(xLeft, yLeft, Color.RED));
             }
         }
         //Right
-        for(int xRight=(armsStartX + 28); xRight<(armsStartX + 5); xRight++) {
+        for(int xRight=(armsStartX + 28); xRight<(armsStartX + 5 + 28); xRight++) {
             for(int yRight=armsStartY; yRight<(armsStartY + 8); yRight++) {
-                rightPoints.put(xRight, yRight);
+                this.rightArm.add(new Storage(xRight, yRight, Color.RED));
             }
         }
 
@@ -138,16 +127,14 @@ public class Meatboy extends CharacterCollideable implements GameCharacter {
         g.fillRect(topMouthStartX + 1, topMouthStartY + 3, 7, 2);
         int bottomMouthX = topMouthStartX + 1;
         int bottomMouthY = topMouthStartY + 3;
-        //Add to irrelevant arrays
-        HashMap<Integer, Integer> blackMouthPoints = new HashMap<>();
-        HashMap<Integer, Integer> whiteMouthPoints = new HashMap<>();
+        //Add to irrelevant array
         //Top
         for(int y=topMouthStartY; y<(topMouthStartY+2); y++) {
             for(int x=topMouthStartX; x<(topMouthStartX+9); x++) {
                 if(canvas.getRGB(x, y) == Color.BLACK.getRGB()) {
-                    blackMouthPoints.put(x, y);
+                    this.irrPoints.add(new Storage(x, y, Color.BLACK));
                 } else {
-                    whiteMouthPoints.put(x, y);
+                    this.irrPoints.add(new Storage(x, y, Color.WHITE));
                 }
             }
         }
@@ -155,20 +142,13 @@ public class Meatboy extends CharacterCollideable implements GameCharacter {
         for(int y=bottomMouthY; y<(bottomMouthY+2); y++) {
             for(int x=bottomMouthX; x<(bottomMouthX+9); x++) {
                 if(canvas.getRGB(x, y) == Color.BLACK.getRGB()) {
-                    blackMouthPoints.put(x, y);
+                    this.irrPoints.add(new Storage(x, y, Color.BLACK));
                 } else {
-                    whiteMouthPoints.put(x, y);
+                    this.irrPoints.add(new Storage(x, y, Color.WHITE));
                 }
             }
         }
-        this.irrelevantPoints.put(whiteMouthPoints, Color.WHITE.getRGB());
-        this.irrelevantPoints.put(blackMouthPoints, Color.BLACK.getRGB());
-
-        //Arms arrays
-        //Left
-        this.leftArm = leftPoints;
-        //Right
-        this.rightArm = rightPoints;
+        //System.out.println("irrelevantPoints HashMap Keyset: " + Arrays.toString(this.irrelevantPoints.keySet().toArray()));
     }
 
     @Override
@@ -185,24 +165,29 @@ public class Meatboy extends CharacterCollideable implements GameCharacter {
             //Reset arms
             resetArms();
             //Draw it
-            for (HashMap<Integer, Integer> hashmap : this.irrelevantPoints.keySet()) {
-                int color = this.irrelevantPoints.get(hashmap);
-                for (int x : hashmap.keySet()) {
-                    int y = hashmap.get(x);
-                    canvas.setRGB(x, y, color);
-                    System.out.println("Drew line color: " + color);
-                }
-            }
-            g.setColor(Color.RED);
-            for (int x : this.leftArm.keySet()) {
-                int y = this.leftArm.get(x);
+            for(Storage pixel : this.irrPoints) {
+                int x = pixel.getX();
+                int y = pixel.getY();
+                Color color = pixel.getColor();
+                g.setColor(color);
                 g.drawLine(x, y, x, y);
-                //System.out.println("Drew line color: RED");
             }
-            for (int x : this.rightArm.keySet()) {
-                int y = this.rightArm.get(x);
+
+            //Left Arm
+            for(Storage pixel : this.leftArm) {
+                int x = pixel.getX();
+                int y = pixel.getY();
+                Color color = pixel.getColor();
+                g.setColor(color);
                 g.drawLine(x, y, x, y);
-                //System.out.println("Drew line color: RED" );
+            }
+            //Right Arm
+            for(Storage pixel : this.rightArm) {
+                int x = pixel.getX();
+                int y = pixel.getY();
+                Color color = pixel.getColor();
+                g.setColor(color);
+                g.drawLine(x, y, x, y);
             }
         }
     }
@@ -272,26 +257,7 @@ public class Meatboy extends CharacterCollideable implements GameCharacter {
         int moveLeftArmDown = 3;
         int moveRightArmUp = 3;
         //IrrelevantPoints
-        for(HashMap<Integer, Integer> hashmap : this.irrelevantPoints.keySet()) {
-            for (Iterator it = hashmap.keySet().iterator(); it.hasNext(); ) {
-                int x = (int) it.next();
-                int y = hashmap.get(x);
-                hashmap.put((x + moveRight), y);
-                it.remove();
-            }
-        }
-        //LeftArm
-        for (Iterator it = this.leftArm.keySet().iterator(); it.hasNext();) {
-            int x = (int) it.next();
-            int y = this.leftArm.get(x);
-            this.leftArm.put((x + moveRight), y); //Maybe make it go down a bit
-        }
-        //RightArm
-        for (Iterator it = this.rightArm.keySet().iterator(); it.hasNext();) {
-            int x = (int) it.next();
-            int y = this.rightArm.get(x);
-            this.rightArm.put((x + moveRight), y); //Maybe make it go up a bit
-        }
+        //
     }
 
     @Override
